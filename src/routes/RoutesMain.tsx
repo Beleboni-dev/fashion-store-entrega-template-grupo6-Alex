515c237a95e43bb35b0a5314c55ff7386781ed0d
Anderson Verrone <doverrone@gmail.com>
2023-07-02 14:38:40 -0300

    feat: Criar routes do projeto e pastas base

---
+++ src/routes/RoutesMain.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import { RegisterPage } from "../pages/RegisterPage";
+import { LoginPage } from "../pages/LoginPage";
+import { HomePage } from "../pages/HomePage";
+import { ProductPage } from "../pages/ProductPage";
+import { AdminPage } from "../pages/AdminPage";
+
+export const RoutesMain = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/product" element={<ProductPage />} />
+            <Route path="/admin" element={<AdminPage />} />
+        </Routes>
+    )
+}
